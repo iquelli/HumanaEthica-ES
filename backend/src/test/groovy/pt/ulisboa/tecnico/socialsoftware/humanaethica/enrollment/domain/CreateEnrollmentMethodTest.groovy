335a91feabfb2f0b151db055b880b94cfccbf296
Catarina Freire <catarina.xie.freire@tecnico.ulisboa.pt>
2024-02-27 21:27:36 +0000

    feat: add test case for Enrollment volunteer's motivation is at least 10 characters invariant

--- backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/humanaethica/enrollment/domain/CreateEnrollmentMethodTest.groovy
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/humanaethica/enrollment/domain/CreateEnrollmentMethodTest.groovy
@@ -5,6 +5,7 @@ import org.springframework.boot.test.context.TestConfiguration
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.BeanConfiguration
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.SpockTest
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.activity.domain.Activity
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.activity.dto.ActivityDto
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.enrollment.dto.EnrollmentDto
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.exceptions.ErrorMessage
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.exceptions.HEException
@@ -45,12 +46,10 @@ class CreateEnrollmentMethodTest extends SpockTest {
         1 * activity.addEnrollment(_)
     }
 
-
     def "create enrollment and violate a volunteer can apply only once to an activity"() {
         given:
         activity.getApplicationDeadline() >> IN_TWO_DAYS
         activity.getName() >> ACTIVITY_NAME_1
-        activity.getEnrollments() >> [otherEnrollment]
         volunteer.getEnrollments() >> [otherEnrollment]
         otherEnrollment.getActivity() >> activity
 
@@ -66,7 +65,6 @@ class CreateEnrollmentMethodTest extends SpockTest {
         given:
         activity.getApplicationDeadline() >> ONE_DAY_AGO
         activity.getName() >> ACTIVITY_NAME_1
-        activity.getEnrollments() >> [otherEnrollment]
         volunteer.getEnrollments() >> [otherEnrollment]
         otherEnrollment.getActivity() >> otherActivity
         otherActivity.getName() >> ACTIVITY_NAME_2
@@ -77,6 +75,26 @@ class CreateEnrollmentMethodTest extends SpockTest {
         then:
         def error = thrown(HEException)
         error.getErrorMessage() == ErrorMessage.ENROLLMENT_PERIOD_CLOSED
+    }
+
+    def "create enrollment and violate volunteers must provide a motivation of at least 10 character"() {
+        given:
+        activity.getApplicationDeadline() >> IN_TWO_DAYS
+        activity.getName() >> ACTIVITY_NAME_1
+        volunteer.getEnrollments() >> [otherEnrollment]
+        otherEnrollment.getActivity() >> otherActivity
+        otherActivity.getName() >> ACTIVITY_NAME_2
+        enrollmentDto.setMotivation(motivation)
+
+        when:
+        new Enrollment(activity, volunteer, enrollmentDto)
+
+        then:
+        def error = thrown(HEException)
+        error.getErrorMessage() == ErrorMessage.ENROLLMENT_MOTIVATION_TOO_SHORT
+
+        where:
+        motivation << [null, "", " ", "motivatio", "12*123!"]
     }
 
     @TestConfiguration
