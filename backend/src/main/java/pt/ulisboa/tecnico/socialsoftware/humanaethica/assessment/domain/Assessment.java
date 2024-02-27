6b645f33cf1de6c9ca6d2dab5913d9f5931113a9
iquelli <raquel.braunschweig@tecnico.ulisboa.pt>
2024-02-27 13:48:26 +0000

    feat: add Assessment and AssessmentDto classes

---
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/assessment/domain/Assessment.java
@@ -0,0 +1,71 @@
+package pt.ulisboa.tecnico.socialsoftware.humanaethica.assessment.domain;
+
+import jakarta.persistence.*;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.institution.domain.Institution;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.user.domain.Volunteer;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "assessment")
+public class Assessment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String review;
+
+    private LocalDateTime reviewDate;
+
+    @ManyToOne
+    private Institution institution;
+
+    @ManyToOne
+    private Volunteer volunteer;
+
+    public Assessment() {
+    }
+
+    // TODO constructor
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getReview() {
+        return this.review;
+    }
+
+    public void setReview(String review) {
+        this.review = review;
+    }
+
+    public LocalDateTime getReviewDate() {
+        return this.reviewDate;
+    }
+
+    public void setReviewDate(LocalDateTime reviewDate) {
+        this.reviewDate = reviewDate;
+    }
+
+    public Institution getInstitution() {
+        return this.institution;
+    }
+
+    public void setInstitution(Institution institution) {
+        this.institution = institution;
+    }
+
+    public Volunteer getVolunteer() {
+        return this.volunteer;
+    }
+
+    public void setVolunteer(Volunteer volunteer) {
+        this.volunteer = volunteer;
+    }
+
+    // private void verifyInvariants() {
+        // TODO
+    // }
+}
+
