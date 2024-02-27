c403677ec3190195d554386f53d78e9cebb0911a
Tiago Farinha <ist1103327@tecnico.ulisboa.pt>
2024-02-27 16:59:09 +0000

    feat: add test case for Enrollment volunteer can only apply once to an activity invariant

--- backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/humanaethica/enrollment/domain/CreateEnrollmentMethodTest.groovy
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/humanaethica/enrollment/domain/CreateEnrollmentMethodTest.groovy
@@ -4,16 +4,12 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.boot.test.context.TestConfiguration
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.BeanConfiguration
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.SpockTest
-import pt.ulisboa.tecnico.socialsoftware.humanaethica.activity.dto.ActivityDto
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.activity.domain.Activity
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.enrollment.dto.EnrollmentDto
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.exceptions.ErrorMessage
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.exceptions.HEException
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.user.domain.Volunteer
-import pt.ulisboa.tecnico.socialsoftware.humanaethica.activity.domain.Activity
-import pt.ulisboa.tecnico.socialsoftware.humanaethica.utils.DateHandler
 import spock.lang.Unroll
-
-import java.time.LocalDateTime
 
 @DataJpaTest
 class CreateEnrollmentMethodTest extends SpockTest {
@@ -49,6 +45,22 @@ class CreateEnrollmentMethodTest extends SpockTest {
         1 * activity.addEnrollment(_)
     }
 
+    @Unroll
+    def "create enrollment and violate a volunteer can apply only once to an activity"() {
+        given:
+        activity.getApplicationDeadline() >> IN_TWO_DAYS
+        activity.getName() >> ACTIVITY_NAME_1
+        activity.getEnrollments() >> [otherEnrollment]
+        volunteer.getEnrollments() >> [otherEnrollment]
+        otherEnrollment.getActivity() >> activity
+
+        when:
+        new Enrollment(activity, volunteer, enrollmentDto)
+
+        then:
+        def error = thrown(HEException)
+        error.getErrorMessage() == ErrorMessage.VOLUNTEER_ALREADY_ENROLLED
+    }
 
     @TestConfiguration
     static class LocalBeanConfiguration extends BeanConfiguration {}
