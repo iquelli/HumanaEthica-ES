83fa8274bfe0522e241b07426cf2ce750c9b8f8b
Catarina Freire <catarina.xie.freire@tecnico.ulisboa.pt>
2024-03-01 18:02:23 +0000

    feat: add POST controller for createEnrollment

---
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/enrollment/EnrollmentController.java
@@ -0,0 +1,29 @@
+package pt.ulisboa.tecnico.socialsoftware.humanaethica.enrollment;
+
+import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.enrollment.dto.EnrollmentDto;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.auth.domain.AuthUser;
+import java.security.Principal;
+
+
+@RestController
+@RequestMapping("/enrollments")
+public class EnrollmentController {
+    @Autowired
+    private EnrollmentService enrollmentService;
+
+    private static final Logger logger = LoggerFactory.getLogger(EnrollmentController.class);
+
+    @PostMapping("/{activityId}")
+    @PreAuthorize("(hasRole('ROLE_VOLUNTEER'))")
+    public EnrollmentDto createEnrollment(Principal principal, @PathVariable Integer activityId, @Valid @RequestBody EnrollmentDto enrollmentDto) {
+        int userId = ((AuthUser) ((Authentication) principal).getPrincipal()).getUser().getId();
+        return enrollmentService.createEnrollment(userId, activityId, enrollmentDto);
+    }
+}
