10ca4173879a6c2de1e37e3c15f8ddc2cfd6af5f
Gonçalo Bárias <goncalo.barias@tecnico.ulisboa.pt>
2024-03-03 17:39:05 +0000

    feat: add POST controller for createAssessment

---
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/assessment/AssessmentController.java
@@ -0,0 +1,31 @@
+package pt.ulisboa.tecnico.socialsoftware.humanaethica.assessment;
+
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.assessment.dto.AssessmentDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.auth.domain.AuthUser;
+
+import java.security.Principal;
+
+@RestController
+@RequestMapping("/assessments")
+public class AssessmentController {
+
+    @Autowired
+    private AssessmentService assessmentService;
+
+    private static final Logger logger = LoggerFactory.getLogger(AssessmentController.class);
+
+    @PostMapping("/{institutionId}")
+    @PreAuthorize("hasRole('ROLE_VOLUNTEER') and hasPermission(#institutionId, 'INSTITUTION.MEMBER')")
+    public AssessmentDto createAssessment(Principal principal, @PathVariable Integer institutionId, @Valid @RequestBody AssessmentDto assessmentDto) {
+        int userId = ((AuthUser) ((Authentication) principal).getPrincipal()).getUser().getId();
+        return assessmentService.createAssessment(userId, institutionId, assessmentDto);
+    }
+
+}
