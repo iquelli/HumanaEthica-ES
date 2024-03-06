1d9ded4e2c7aab395c58d59370d740748edee9e2
Catarina Freire <catarina.xie.freire@tecnico.ulisboa.pt>
2024-03-06 22:13:10 +0000

    feat: add test cases for access conditions violation in getEnrollmentsByActivity web service

--- backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/humanaethica/enrollment/webservice/GetEnrollmentsWebServiceIT.groovy
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/humanaethica/enrollment/webservice/GetEnrollmentsWebServiceIT.groovy
@@ -3,13 +3,16 @@ package pt.ulisboa.tecnico.socialsoftware.humanaethica.enrollment.webservice
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.web.server.LocalServerPort
 import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.WebClientResponseException
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.SpockTest
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.activity.domain.Activity
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.auth.domain.AuthUser
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.enrollment.domain.Enrollment
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.enrollment.dto.EnrollmentDto
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.institution.domain.Institution
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.user.domain.User
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -56,6 +59,9 @@ class GetEnrollmentsWebServiceIT extends SpockTest {
     }
 
     def "get activities"() {
+        given: 'a member'
+        demoMemberLogin()
+
         when:
         def response = webClient.get()
                 .uri('/enrollments/' + activity.getId())
@@ -75,4 +81,72 @@ class GetEnrollmentsWebServiceIT extends SpockTest {
         cleanup:
         deleteAll()
     }
+
+    def "login as member of another institution and get activities"() {
+        given: 'another member'
+        def otherInstitution = new Institution(INSTITUTION_1_NAME, INSTITUTION_1_EMAIL, INSTITUTION_1_NIF)
+        institutionRepository.save(otherInstitution)
+        def otherMember = createMember(USER_3_NAME, USER_3_USERNAME, USER_3_PASSWORD, USER_3_EMAIL, AuthUser.Type.NORMAL,
+                otherInstitution, User.State.APPROVED)
+        normalUserLogin(USER_3_USERNAME, USER_3_PASSWORD)
+
+        when:
+        webClient.get()
+                .uri('/enrollments/' + activity.getId())
+                .headers(httpHeaders -> httpHeaders.putAll(headers))
+                .retrieve()
+                .bodyToFlux(EnrollmentDto.class)
+                .collectList()
+                .block()
+
+        then: "check response status"
+        def error = thrown(WebClientResponseException)
+        error.statusCode == HttpStatus.FORBIDDEN
+
+        cleanup:
+        deleteAll()
+    }
+
+    def "login as volunteer, and get activities"() {
+        given: 'a volunteer'
+        demoVolunteerLogin()
+
+        when:
+        webClient.get()
+                .uri('/enrollments/' + activity.getId())
+                .headers(httpHeaders -> httpHeaders.putAll(headers))
+                .retrieve()
+                .bodyToFlux(EnrollmentDto.class)
+                .collectList()
+                .block()
+
+        then: "check response status"
+        def error = thrown(WebClientResponseException)
+        error.statusCode == HttpStatus.FORBIDDEN
+
+        cleanup:
+        deleteAll()
+    }
+
+    def "login as admin, and get activities"() {
+        given: 'an admin'
+        demoAdminLogin()
+
+        when:
+        webClient.get()
+                .uri('/enrollments/' + activity.getId())
+                .headers(httpHeaders -> httpHeaders.putAll(headers))
+                .retrieve()
+                .bodyToFlux(EnrollmentDto.class)
+                .collectList()
+                .block()
+
+        then: "check response status"
+        def error = thrown(WebClientResponseException)
+        error.statusCode == HttpStatus.FORBIDDEN
+
+        cleanup:
+        deleteAll()
+    }
+
 }
