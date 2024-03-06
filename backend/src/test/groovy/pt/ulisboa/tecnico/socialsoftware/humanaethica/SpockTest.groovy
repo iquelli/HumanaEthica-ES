b7639d4c871363b7873708f0a7223eaf814484b1
Raquel Braunschweig <raquel.braunschweig@tecnico.ulisboa.pt>
2024-03-06 11:52:45 +0000

    feat: add test case for successful getAssessmentsByInstitution service

--- backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/humanaethica/SpockTest.groovy
+++ backend/src/test/groovy/pt/ulisboa/tecnico/socialsoftware/humanaethica/SpockTest.groovy
@@ -227,6 +227,7 @@ class SpockTest extends Specification {
     // assessment
 
     public static final String ASSESSMENT_REVIEW_1 = "assessment review 1"
+    public static final String ASSESSMENT_REVIEW_2 = "assessment review 2"
 
     @Autowired
     AssessmentRepository assessmentRepository
