23c209a51aab14ac1c1f0dff4bcea7c4462ccca3
Raquel Braunschweig <raquel.braunschweig@tecnico.ulisboa.pt>
2024-03-06 12:10:56 +0000

    feat: add GET controller for getAssessmentsByInstitution

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/assessment/AssessmentController.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/assessment/AssessmentController.java
@@ -7,8 +7,10 @@ import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.assessment.dto.AssessmentDto;
 import pt.ulisboa.tecnico.socialsoftware.humanaethica.auth.domain.AuthUser;
+import pt.ulisboa.tecnico.socialsoftware.humanaethica.enrollment.dto.EnrollmentDto;
 
 import java.security.Principal;
+import java.util.List;
 
 @RestController
 @RequestMapping("/assessments")
@@ -18,11 +20,16 @@ public class AssessmentController {
     private AssessmentService assessmentService;
 
     @PostMapping("/{institutionId}")
-    @PreAuthorize("(hasRole('ROLE_VOLUNTEER'))") 
-    public AssessmentDto createAssessment(Principal principal, @PathVariable Integer institutionId, 
+    @PreAuthorize("(hasRole('ROLE_VOLUNTEER'))")
+    public AssessmentDto createAssessment(Principal principal, @PathVariable Integer institutionId,
             @Valid @RequestBody AssessmentDto assessmentDto) {
         int userId = ((AuthUser) ((Authentication) principal).getPrincipal()).getUser().getId();
         return assessmentService.createAssessment(userId, institutionId, assessmentDto);
+    }
+
+    @PutMapping("/{institutionId}")
+    public List<AssessmentDto> getAssessmentsByInstitution(@PathVariable int institutionId) {
+        return assessmentService.getAssessmentsByInstitution(institutionId);
     }
 
 }
