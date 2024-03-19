2741fb017e6902563be39cd93dd313780a66f9e8
Gonçalo Bárias <goncalo.barias@tecnico.ulisboa.pt>
2024-03-19 22:20:33 +0000

    feat: add presentation to the AssessmentDialog

--- frontend/src/views/volunteer/AssessmentDialog.vue
+++ frontend/src/views/volunteer/AssessmentDialog.vue
@@ -1,11 +1,38 @@
 <template>
-  <div><!-- TODO --></div>
+  <v-dialog v-model="dialog" persistent width="1300">
+    <v-card>
+      <v-card-title>
+        <span class="headline"> New Assessment </span>
+      </v-card-title>
+      <v-card-text>
+        <v-form ref="form" lazy-validation>
+          <v-row>
+            <v-col cols="12">
+              <v-text-field label="*Review"></v-text-field>
+            </v-col>
+          </v-row>
+        </v-form>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn
+          color="blue-darken-1"
+          variant="text"
+          @click="$emit('close-assessment-dialog')"
+        >
+          Close
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
 </template>
 <script lang="ts">
-import { Vue, Component } from 'vue-property-decorator';
+import { Vue, Component, Model } from 'vue-property-decorator';
 
 @Component({})
-export default class AssessmentDialog extends Vue {}
+export default class AssessmentDialog extends Vue {
+  @Model('dialog', Boolean) dialog!: boolean;
+}
 </script>
 
 <style scoped lang="scss"></style>
