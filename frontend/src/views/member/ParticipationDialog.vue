d2f6757d9fd344a64230899465ff3a88e63558c0
Francisco Leitão <francisco.eira.leitao@tecnico.ulisboa.pt>
2024-03-21 19:35:45 +0000

    feat: add select participant dialog presentation, initialization and enabling verification behaviour
    Closes #97, #98 and #99

---
+++ frontend/src/views/member/ParticipationDialog.vue
@@ -0,0 +1,86 @@
+<template>
+  <v-dialog v-model="dialog" persistent width="600">
+    <v-card>
+      <v-card-title>
+        <span class="headline"> Select Participant </span>
+      </v-card-title>
+      <v-card-text>
+        <v-form ref="form" lazy-validation>
+          <v-row>
+            <v-col cols="12">
+              <v-text-field
+                label="*Rating"
+                :rules="[
+                  (v) =>
+                    isNumberValid(v) || 'Rating between 1 and 5 is required',
+                ]"
+                required
+                v-model="participation.rating"
+              ></v-text-field>
+            </v-col>
+          </v-row>
+        </v-form>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn
+          color="blue-darken-1"
+          variant="text"
+          @click="$emit('close-participation-dialog')"
+        >
+          Close
+        </v-btn>
+        <v-btn color="blue-darken-1" variant="text" @click="makeParticipant">
+          Make Participant
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+<script lang="ts">
+import { Vue, Component, Prop, Model } from 'vue-property-decorator';
+import RemoteServices from '@/services/RemoteServices';
+import Participation from '@/models/participation/Participation';
+import Enrollment from '@/models/enrollment/Enrollment';
+import Activity from '@/models/activity/Activity';
+
+@Component({})
+export default class ParticipationDialog extends Vue {
+  @Model('dialog', Boolean) dialog!: boolean;
+  @Prop({ type: Enrollment, required: true }) readonly enrollment!: Enrollment;
+  @Prop({ type: Activity, required: true }) readonly activity!: Activity;
+
+  participation: Participation = new Participation();
+
+  async created() {
+    // TODO: criar participation a partir de enrollment recebido
+    // remover isto?
+    this.participation = new Participation();
+  }
+
+  isNumberValid(value: any) {
+    if (!/^\d+$/.test(value)) return false;
+    const parsedValue = parseInt(value);
+    return parsedValue >= 1 && parsedValue <= 5;
+  }
+
+  async makeParticipant() {
+    if ((this.$refs.form as Vue & { validate: () => boolean }).validate()) {
+      if (this.activity.id !== null) {
+        try {
+          const result = await RemoteServices.createParticipation(
+            this.$store.getters.getUser.id,
+            this.activity.id,
+            this.participation,
+          );
+          this.$emit('save-participation', result);
+        } catch (error) {
+          await this.$store.dispatch('error', error);
+        }
+      }
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss"></style>
