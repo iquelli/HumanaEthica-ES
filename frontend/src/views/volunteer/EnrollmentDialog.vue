2f910f4eb24dbe6b0d36cc4f13948abe60403eab
Tiago Farinha <ist1103327@tecnico.ulisboa.pt>
2024-03-22 21:16:59 +0000

    feat: add apply to activity dialog presentation
    realted to #81

---
+++ frontend/src/views/volunteer/EnrollmentDialog.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-dialog v-model="dialog" persistent width="1300">
+    <v-card>
+      <v-card-title>
+        <span class="headline">
+            New Application
+        </span>
+      </v-card-title>
+      <v-card-text>
+        <v-form ref="form" lazy-validation>
+          <v-row>
+            <v-col cols="12">
+              <v-text-field label="*Motivation"></v-text-field>
+            </v-col>
+          </v-row>
+        </v-form>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn
+            color="blue-darken-1"
+            variant="text"
+            @click="$emit('close-enrollment-dialog')"
+        >
+          Close
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+<script lang="ts">
+import {Vue, Component, Model} from 'vue-property-decorator';
+
+@Component({})
+export default class EnrollmentDialog extends Vue {
+  @Model('dialog', Boolean) dialog!: boolean;
+
+}
+</script>
+
+<style scoped lang="scss"></style>
