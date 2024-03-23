017340262a1908d1a592a2cb7cb98c7c83b0451a
Catarina Freire <catarina.xie.freire@tecnico.ulisboa.pt>
2024-03-23 20:46:48 +0000

    fix: formatting issues

--- backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/activity/dto/ActivityDto.java
+++ backend/src/main/java/pt/ulisboa/tecnico/socialsoftware/humanaethica/activity/dto/ActivityDto.java
@@ -12,7 +12,7 @@ public class ActivityDto {
     private String name;
     private String region;
     private Integer participantsNumberLimit;
-	private Integer numberOfEnrollments;
+    private Integer numberOfEnrollments;
     private String description;
     private String startingDate;
     private String endingDate;
@@ -30,7 +30,7 @@ public class ActivityDto {
         setName(activity.getName());
         setRegion(activity.getRegion());
         setParticipantsNumberLimit(activity.getParticipantsNumberLimit());
-		setNumberOfEnrollments(activity.getEnrollments().size());
+        setNumberOfEnrollments(activity.getEnrollments().size());
         setDescription(activity.getDescription());
 
         this.themes = activity.getThemes().stream()
